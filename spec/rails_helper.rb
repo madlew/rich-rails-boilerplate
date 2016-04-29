28d4cb9f5adb7c6a9188a076da6a4e549375cb6d
Lukasz Madlewski <lukasz.madlewski@u2i.com>
2016-04-29 10:39:31 +0200

    Add Shoulda:Matchers basic config

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -60,3 +60,10 @@ RSpec.configure do |config|
   # config.filter_gems_from_backtrace("gem name")
   config.fuubar_progress_bar_options = { format: '%a (%E): |%b>%i| %p%% (%c/%C)' }
 end
+
+Shoulda::Matchers.configure do |config|
+  config.integrate do |with|
+    with.test_framework :rspec
+    with.library :rails
+  end
+end
